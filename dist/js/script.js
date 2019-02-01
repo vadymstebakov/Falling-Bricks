a4c41fd5c4fdd840da55a8f85b7696f849db21a6
Vadym <vadim.stebakov.94@gmail.com>
2019-02-01 23:46:44 +0200

    fix animate

--- dist/js/script.js
+++ dist/js/script.js
@@ -13,86 +13,74 @@ document.addEventListener('DOMContentLoaded', function () {
   var workWidth = screenWidth - elBrick.offsetWidth;
   var workHeight = screenHeight + elBrick.offsetHeight;
 
-  // Move Bucket
-  function moveBucket() {
-    elBucket.addEventListener('mousedown', function (e) {
-      var backetCoords = getCoords(elBucket);
-      var shiftX = e.pageX - backetCoords.left;
-      var bgCoords = getCoords(elBg);
-
-      document.onmousemove = function (e) {
-        var newLeft = e.pageX - shiftX - bgCoords.left;
-        var rightEdge = elBg.offsetWidth - elBucket.offsetWidth;
-
-        if (newLeft < 0) {
-          newLeft = 0;
-        }
-        if (newLeft > rightEdge) {
-          newLeft = rightEdge;
-        }
-        elBucket.style.left = newLeft + 'px';
-      };
-
-      document.onmouseup = function () {
-        document.onmousemove = document.onmouseup = null;
-      };
-      return false;
-    });
-
-    elBucket.ondragstart = function () {
-      return false;
-    };
-
-    function getCoords(elem) {
-      var box = elem.getBoundingClientRect();
-
-      return {
-        top: box.top + pageYOffset,
-        left: box.left + pageXOffset };
-
-    }
-  }
-  moveBucket();
-
-  // Randomizer
-  function mtRand(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
-  // Animate
-  function animate(_ref) {var timing = _ref.timing,draw = _ref.draw,duration = _ref.duration;
-    var start = performance.now();
-
-    requestAnimationFrame(function animate(time) {
-      var timeFraction = (time - start) / duration;
-      if (timeFraction > 1) timeFraction = 1;
-
-      var progress = timing(timeFraction);
-
-      draw(progress);
-
-      if (timeFraction < 1) {
-        requestAnimationFrame(animate);
-      }
-    });
-  }
-
-  function makeLinear(timing) {
-    return function (timeFraction) {
-      return 1 - timing(1 - timeFraction);
-    };
-  }
-
-  function linear(timeFraction) {
-    return timeFraction;
-  }
-
-  var anLinear = makeLinear(linear);
-
-  elBrick.style.left = mtRand(0, workWidth) + 'px';
-
-  //Fall Bricks
   elBucket.addEventListener('animationend', function () {
+    // Move Bucket
+    function moveBucket() {
+      elBucket.addEventListener('mousedown', function (e) {
+        var backetCoords = getCoords(elBucket);
+        var shiftX = e.pageX - backetCoords.left;
+        var bgCoords = getCoords(elBg);
+
+        document.onmousemove = function (e) {
+          var newLeft = e.pageX - shiftX - bgCoords.left;
+          var rightEdge = elBg.offsetWidth - elBucket.offsetWidth;
+
+          if (newLeft < 0) {
+            newLeft = 0;
+          }
+          if (newLeft > rightEdge) {
+            newLeft = rightEdge;
+          }
+          elBucket.style.left = newLeft + 'px';
+        };
+
+        document.onmouseup = function () {
+          document.onmousemove = document.onmouseup = null;
+        };
+        return false;
+      });
+
+      elBucket.ondragstart = function () {
+        return false;
+      };
+
+      function getCoords(elem) {
+        var box = elem.getBoundingClientRect();
+
+        return {
+          top: box.top + pageYOffset,
+          left: box.left + pageXOffset };
+
+      }
+    }
+    moveBucket();
+
+    // Randomizer
+    function mtRand(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    // Animate
+    function animate(_ref) {var timing = _ref.timing,draw = _ref.draw,duration = _ref.duration;
+      var start = performance.now();
+
+      requestAnimationFrame(function animate(time) {
+        var timeFraction = (time - start) / duration;
+        if (timeFraction > 1) timeFraction = 1;
+
+        var progress = timing(timeFraction);
+
+        draw(progress);
+
+        if (timeFraction < 1) {
+          requestAnimationFrame(animate);
+        }
+      });
+    }
+
+    elBrick.style.left = mtRand(0, workWidth) + 'px';
+
+    //Fall Bricks
     function fall() {
       var elBricks = document.querySelectorAll('.brick');
       var lengthBricks = elBricks.length;var _loop = function _loop(
@@ -101,7 +89,9 @@ document.addEventListener('DOMContentLoaded', function () {
         var thisBrick = elBricks[i];
         animate({
           duration: mtRand(2500, 9000),
-          timing: anLinear,
+          timing: function linear(timeFraction) {
+            return timeFraction;
+          },
           draw: function draw(progress) {
             thisBrick.style.top = progress * workHeight + 'px';
 
--- src/js/script.js
+++ src/js/script.js
@@ -13,86 +13,74 @@ document.addEventListener('DOMContentLoaded', function() {
 	let workWidth = screenWidth - elBrick.offsetWidth;
 	let workHeight = screenHeight + elBrick.offsetHeight;
 
-	// Move Bucket
-	function moveBucket() {
-		elBucket.addEventListener('mousedown', function(e) {
-			let backetCoords = getCoords(elBucket);
-			let shiftX = e.pageX - backetCoords.left;
-			let bgCoords = getCoords(elBg);
-			
-			document.onmousemove = function(e) {
-				let newLeft = e.pageX - shiftX - bgCoords.left;
-				let rightEdge = elBg.offsetWidth - elBucket.offsetWidth;
-		
-				if (newLeft < 0) {
-					newLeft = 0;
-				}
-				if (newLeft > rightEdge) {
-					newLeft = rightEdge;
-				}
-				elBucket.style.left = newLeft + 'px';
-			};
-
-			document.onmouseup = function() {
-				document.onmousemove = document.onmouseup = null;
-			};
-			return false;
-		});
-
-		elBucket.ondragstart = function() {
-			return false;
-		};
-
-		function getCoords(elem) {
-			var box = elem.getBoundingClientRect();
-
-			return {
-				top: box.top + pageYOffset,
-				left: box.left + pageXOffset
-			};
-		}
-	}
-	moveBucket();
-
-	// Randomizer
-	function mtRand(min, max) {
-		return Math.floor(Math.random() * (max - min + 1) + min);
-	} 
-
-	// Animate
-	function animate({timing, draw, duration}) {
-		let start = performance.now();
-		
-		requestAnimationFrame(function animate(time) {
-			let timeFraction = (time - start) / duration;
-			if (timeFraction > 1) timeFraction = 1;
-		
-			let progress = timing(timeFraction);
-		
-			draw(progress);
-		
-			if (timeFraction < 1) {
-				requestAnimationFrame(animate);
-			}
-		});
-	}
-
-	function makeLinear(timing) {
-		return function(timeFraction) {
-			return 1 - timing(1 - timeFraction);
-		};
-	}
-  
-	function linear(timeFraction) {
-		return timeFraction;
-	}
-  
-	let anLinear = makeLinear(linear);
-
-	elBrick.style.left = mtRand(0, workWidth) + 'px';
-
-	//Fall Bricks
 	elBucket.addEventListener('animationend', function() {
+		// Move Bucket
+		function moveBucket() {
+			elBucket.addEventListener('mousedown', function(e) {
+				let backetCoords = getCoords(elBucket);
+				let shiftX = e.pageX - backetCoords.left;
+				let bgCoords = getCoords(elBg);
+				
+				document.onmousemove = function(e) {
+					let newLeft = e.pageX - shiftX - bgCoords.left;
+					let rightEdge = elBg.offsetWidth - elBucket.offsetWidth;
+			
+					if (newLeft < 0) {
+						newLeft = 0;
+					}
+					if (newLeft > rightEdge) {
+						newLeft = rightEdge;
+					}
+					elBucket.style.left = newLeft + 'px';
+				};
+
+				document.onmouseup = function() {
+					document.onmousemove = document.onmouseup = null;
+				};
+				return false;
+			});
+
+			elBucket.ondragstart = function() {
+				return false;
+			};
+
+			function getCoords(elem) {
+				var box = elem.getBoundingClientRect();
+
+				return {
+					top: box.top + pageYOffset,
+					left: box.left + pageXOffset
+				};
+			}
+		}
+		moveBucket();
+
+		// Randomizer
+		function mtRand(min, max) {
+			return Math.floor(Math.random() * (max - min + 1) + min);
+		} 
+
+		// Animate
+		function animate({timing, draw, duration}) {
+			let start = performance.now();
+			
+			requestAnimationFrame(function animate(time) {
+				let timeFraction = (time - start) / duration;
+				if (timeFraction > 1) timeFraction = 1;
+			
+				let progress = timing(timeFraction);
+			
+				draw(progress);
+			
+				if (timeFraction < 1) {
+					requestAnimationFrame(animate);
+				}
+			});
+		}
+
+		elBrick.style.left = mtRand(0, workWidth) + 'px';
+
+		//Fall Bricks
 		function fall() {
 			let elBricks = document.querySelectorAll('.brick');
 			let lengthBricks = elBricks.length;
@@ -101,7 +89,9 @@ document.addEventListener('DOMContentLoaded', function() {
 				let thisBrick = elBricks[i];
 				animate({
 					duration: mtRand(2500, 9000),
-					timing: anLinear,
+					timing: function linear(timeFraction) {
+						return timeFraction;
+					},
 					draw: function(progress) {
 						thisBrick.style.top = progress * workHeight  + 'px';
 						
@@ -130,7 +120,7 @@ document.addEventListener('DOMContentLoaded', function() {
 			newBrick.className = 'brick';
 			elBg.appendChild(newBrick);
 		}
-		
+			
 	});
 
 	
